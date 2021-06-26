4924046b13dc70abbd9b7d946bd2be7ae9f9d7f4
h4ndzdatm0ld <hugotinoco@icloud.com>
2021-06-26 00:02:30 -0700

    unit test work

--- tests/unit/test_pyxl_data_map.py
+++ tests/unit/test_pyxl_data_map.py
@@ -3,33 +3,48 @@
 import os
 
 from nornir_pyxl.plugins.tasks.pyxl_data_map import pyxl_data_map
-from tests.conftest import NestedDataMap, DataMap  # Class fixtures not supported.
+from tests.conftest import (
+    NestedDataMap,
+    DataMap,
+    DIR_PATH,
+)  # Class fixtures not supported.
 
-data_dir = f"{os.path.dirname(os.path.realpath(__file__))}/test_data"
-
-WORKING_WORKBOOK = f"{data_dir}/working_example_wb.xlsx"
+WORKING_WORKBOOK = f"{DIR_PATH}/unit/test_data/working_example_wb.xlsx"
 SHEETNAME = "IP_DATA"
 
-
-def test_nested_dict(nornir, nested_dict):
-    """Test to ensure the nested key with position 0 is the name of the outer dictionary."""
-    data = nornir.run(
-        task=pyxl_data_map,
-        workbook=WORKING_WORKBOOK,
-        sheetname=SHEETNAME,
-        mapping=NestedDataMap,
-        min_row=2,
-    )
-    assert data["test-nomad"][0].result == nested_dict
+# If NORNIR_LOG set to True, the log won't be deleted in teardown.
+nornir_logfile = os.environ.get("NORNIR_LOG", False)
 
 
-def test_dict(nornir, list_of_dicts):
-    """Test to ensure the nested key with position 0 is the name of the outer dictionary."""
-    data = nornir.run(
-        task=pyxl_data_map,
-        workbook=WORKING_WORKBOOK,
-        sheetname=SHEETNAME,
-        mapping=DataMap,
-        min_row=2,
-    )
-    assert data["test-nomad"][0].result == list_of_dicts
+class TestNornirDataMap:
+    """Test Class for DataMap"""
+
+    def teardown_class(self):
+        """Teardown."""
+        # Remove nornir_log
+        if not nornir_logfile:
+            nornir_log = f"{DIR_PATH}/nornir_test.log"
+            if os.path.exists(nornir_log):
+                os.remove(nornir_log)
+
+    def test_nested_dict(self, nornir, nested_dict):
+        """Test to ensure the nested key with position 0 is the name of the outer dictionary."""
+        data = nornir.run(
+            task=pyxl_data_map,
+            workbook=WORKING_WORKBOOK,
+            sheetname=SHEETNAME,
+            mapping=NestedDataMap,
+            min_row=2,
+        )
+        assert data["test-nomad"][0].result == nested_dict
+
+    def test_dict(self, nornir, list_of_dicts):
+        """Test to ensure the nested key with position 0 is the name of the outer dictionary."""
+        data = nornir.run(
+            task=pyxl_data_map,
+            workbook=WORKING_WORKBOOK,
+            sheetname=SHEETNAME,
+            mapping=DataMap,
+            min_row=2,
+        )
+        assert data["test-nomad"][0].result == list_of_dicts
